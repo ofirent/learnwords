8f7d3659bae80c4685be9ba3506588f23eee3111
shalev ofir <50710665+ofirent@users.noreply.github.com>
2025-11-28 11:48:04 +0200

    Update appState.ts

--- src/appState.ts
+++ src/appState.ts
@@ -1,14 +1,16 @@
 import * as React from 'react'
 import type { VocabItem } from './types'
 
-type AppState = {
-  tab: 'learn' | 'practice' | 'quiz' | 'srs' | 'practiceHard'
-  setTab: (t: AppState['tab']) => void
+export type TabKey = 'learn' | 'practice' | 'quiz' | 'srs' | 'practiceHard'
+
+interface AppContextValue {
+  tab: TabKey
+  setTab: (tab: TabKey) => void
   hardWords: VocabItem[]
-  setHardWords: (w: VocabItem[]) => void
+  setHardWords: React.Dispatch<React.SetStateAction<VocabItem[]>>
 }
 
-export const AppContext = React.createContext<AppState>({
+export const AppContext = React.createContext<AppContextValue>({
   tab: 'learn',
   setTab: () => {},
   hardWords: [],
